4841337a0204be5972d52923075165896ed0e783
joanaagbayani <joana.hasel.agbayani@aclcbutuan.edu.ph>
2025-02-19 23:20:27 +0800

    Add files via upload

---
+++ console.py
@@ -0,0 +1,5 @@
+firstname = input("Enter your firstname: ")
+lastname = input("Enter your lastname: ")
+age = input("Enter your age: ")
+
+print(f"Hi I would like to introduce myself, Im, {firstname} {lastname}, you are {age} years old!")
